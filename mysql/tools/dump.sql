f8a926596391573b525fed6da8be53c068ad121b
karroch-a <abdellah.azeroual.info@gmail.com>
2022-06-08 01:39:16 +0100

    update

--- mysql/tools/dump.sql
+++ mysql/tools/dump.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE karroch;
-CREATE USER 'aazeroua'@'%' identified by 'karroch';
+CREATE USER 'aazeroua'@'%' identified WITH mysql_native_password by 'karroch';
 GRANT ALL PRIVILEGES ON karroch.* TO 'aazeroua'@'%';
 FLUSH PRIVILEGES;
